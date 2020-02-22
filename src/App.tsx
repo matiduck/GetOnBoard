f2115e3efe454b08aa4f351cdaeacf2be39fc84b
Mateusz Kaczor <mk16316@st.amu.edu.pl>
2020-02-22 11:43:59 +0100

    * Adding default GoB theme * switching spaces to tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import './App.scss';
 import { Header } from './Components/Header/Header';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { goBDefaultTheme } from './Themes/GoBDefaultTheme';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header>
-        <Header />
-      </header>
-      <body>
-        <h1 className="h2">Body</h1>
-      </body>
-    </div>
-  );
+	return (
+		<div className="App">
+			<ThemeProvider theme={goBDefaultTheme}>
+				<header>
+					<Header />
+				</header>
+				<body>
+					<h1 className="h2">Body</h1>
+				</body>
+			</ThemeProvider>
+		</div>
+	);
 };
 
 export default App;
---
+++ src/Themes/GoBDefaultTheme.ts
@@ -0,0 +1,9 @@
+import { createMuiTheme } from '@material-ui/core';
+
+export const goBDefaultTheme = createMuiTheme({
+	palette: {
+		background: {
+			default: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+		},
+	},
+});
